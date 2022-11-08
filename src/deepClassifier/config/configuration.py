f8c885e6f79e36acde3cefdfac2254a7cf1657c1
fazlullahb0 <fazlullahb0@gmail.com>
2022-11-08 10:40:37 +0530

    stage 2

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories 
-from deepClassifier.entity import DataIngestionConfig 
+from deepClassifier.entity import DataIngestionConfig, PrepareBaseModelConfig 
+from pathlib import Path 
 
 
 
@@ -11,7 +12,25 @@ class ConfigurationManager:
         params_filepath=PARAMS_FILE_PATH):
         self.config=read_yaml(config_filepath)
         self.params=read_yaml(params_filepath)
-        create_directories([self.config.artifacts_root])
+        create_directories([self.config.artifacts_root]) 
+        
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
@@ -24,4 +43,4 @@ class ConfigurationManager:
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir)
 
-        return data_ingestion_config
+        return data_ingestion_config 
