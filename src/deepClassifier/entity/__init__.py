64234a91d14cf3d671153af3d82fc1d025607d06
fazlullahb0 <fazlullahb0@gmail.com>
2022-11-16 13:12:09 +0530

    hh

--- src/deepClassifier/entity/__init__.py
+++ src/deepClassifier/entity/__init__.py
@@ -1,5 +1,6 @@
 from deepClassifier.entity.config_entity import (DataIngestionConfig, 
                                                  PrepareBaseModelConfig, 
                                                  PrepareCallbacksConfig,
-                                                 TrainingConfig)
+                                                 TrainingConfig,
+                                                 EvaluationConfig)
 
